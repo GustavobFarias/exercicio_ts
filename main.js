c8986a533417d3d0bb974ff15160af59eb4ae573
Gustavo Farias <gustavoburgerf@gmail.com>
2024-10-31 17:02:23 -0300

    exercicio do typeScript

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiplicar(5, 5));
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOla("Gustavo"));
---
+++ main.ts
@@ -0,0 +1,11 @@
+function multiplicar(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+console.log(multiplicar(5, 5));
+
+function dizOla(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(dizOla("Gustavo"));
